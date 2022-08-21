ac05098930f342c7de4a54cdd43f073056ace614
Kelliton Santana De Souza <programmerKelliton@gmail.com>
2022-08-21 19:39:51 -0300

    Estruturando html e adicionando cores

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,27 @@
 import "./App.scss";
+import ImgPerson from "../../public/images/image-victor.jpg";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <main className="container">
+                <div className="container__background"></div>
+                <div className="card">
+                    <img
+                        className="card__person"
+                        src={ImgPerson}
+                        alt="One person image"
+                    />
+                    <div className="info">
+                        <h1 className="info__name">Victor Crest</h1>
+                        <span className="info__age">26</span>
+                    </div>
+                    <span className="card__country">London</span>
+                    <div className="card__divider"></div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
